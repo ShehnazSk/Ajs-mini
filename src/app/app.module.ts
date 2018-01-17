f245816c07f7a07a8616d2d4017ac01570fc4ce4
ShehnazSk <shehnazshaik7@gmail.com>
2018-01-17 21:57:32 +0530

    Angular basic project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NewCmpComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
