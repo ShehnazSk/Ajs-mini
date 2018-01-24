d614708f3e9f8c7978091d280f0ba18f96d6e138
ShehnazSk <shehnazshaik7@gmail.com>
2018-01-24 23:21:02 +0530

    Resume-first app using angular -4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { NewCmpComponent } from './new-cmp/new-cmp.component';
+import { MenuComponent } from './menu/menu.component';
+import { BodyComponent } from './body/body.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+
+
+
+
+const myRoutes: Routes = [
+  { path:'',component:BodyComponent },
+  { path:'experience',component: ExperienceComponent },
+  { path:'education',component: EducationComponent },
+  { path:'Skills',component: SkillsComponent },
+  { path:'projects',component: ProjectsComponent },
+  { path:'contact',component: ContactComponent }
+ 
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    NewCmpComponent
+    MenuComponent,
+    BodyComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
